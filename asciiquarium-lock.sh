31e2d097eceb83921e1f1f34cd3fcfe063d98ed4
G4bbix <winkiway@gmail.com>
2021-11-02 16:14:08 +0100

    Added capability for scaled screens, changed to xtrlock, improved code readability

--- asciiquarium-lock.sh
+++ asciiquarium-lock.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
+
 # Dependency checks
-if [ ! $(command -v asciiquarium) ]; then
-    echo "\"command not found: \"asciiquarium\"" >&2
-    exit 1
+if [ ! "$(command -v asciiquarium)" ]; then
+  echo "\"command not found: \"asciiquarium\"" >&2
+  exit 1
 fi
-if [ ! $(command -v xfce4-terminal) ]; then
-    echo "\"command not found: \"xfce4-terminal\"" >&2
-    exit 1
+if [ ! "$(command -v xfce4-terminal)" ]; then
+  echo "\"command not found: \"xfce4-terminal\"" >&2
+  exit 1
 fi
-if [ ! $(command -v pyxtrlock) ]; then
-    echo "\"command not found: \"pyxtrlock\"" >&2
-    exit 1
+if [ ! "$(command -v xtrlock)" ]; then
+  echo "\"command not found: \"xtrlock\"" >&2
+  exit 1
 fi
 
 # Check for xrandr and run xterms
-if [ $(command -v xrandr) ]; then
-    OFFSETS=$(\xrandr | \grep -w connected | \awk '{split($0, line, " "); if (line[3] == "primary") { split(line[4], offsets, "+"); print "+"offsets[2]"+"offsets[3] } else { split(line[3], offsets, "+"); print "+"offsets[2]"+"offsets[3] }}')
-    for o in $OFFSETS; do
-        \xfce4-terminal --geometry 0x0$o --fullscreen --hide-menubar --hide-scrollbar --hide-borders -e asciiquarium &
-    done
-else
-    xterm -fullscreen -e asciiquarium &
-fi
+if [ "$(command -v xrandr)" ]; then
 
-# Store the current layout and set the default one
-if [ $(command -v xkblayout-state) ]; then
-    CURRENT=$(xkblayout-state print %c)
-    xkblayout-state set 0
+  # Calculate scaled offsets
+  ORIGINAL_RES="$(xrandr | grep -w connected | grep -oE '[0-9]{4}x[0-9]{4}' | head -n -1)"
+  SCALING="$(grep -m 1 "<scale>" ~/.config/monitors.xml  | grep -oE '[0-9]')"
+  OFFSETS="+0+0"
+  PREV_X=0
+  while read -r RES ; do
+    X=$((PREV_X + $(echo "$RES" | cut -d x -f1) / SCALING))
+    PREV_X="$X"
+    OFFSETS="${OFFSETS};+${X}+0"
+  done <<< "$ORIGINAL_RES"
+
+  IFS=";"
+  for o in $OFFSETS; do
+     xfce4-terminal --geometry 0x0$o --fullscreen --hide-menubar \
+      --hide-scrollbar --hide-borders -e asciiquarium &
+  done
+else
+  xterm -fullscreen -e asciiquarium &
 fi
 
 #Lock screen and kill all the child processes on unlock
-pyxtrlock && pkill -P $$
-
-#Restore kb layout
-if [ $(command -v xkblayout-state) ]; then
-    xkblayout-state set $CURRENT
-fi
+xtrlock && pkill -f asciiquarium
