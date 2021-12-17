98484adcd012baf29367c2861cf87a6e488fc3ae
G4bbix <winkiway@gmail.com>
2021-12-17 09:28:58 +0100

    Move to i3

--- asciiquarium-lock.sh
+++ asciiquarium-lock.sh
@@ -13,32 +13,15 @@ if [ ! "$(command -v xtrlock)" ]; then
   echo "\"command not found: \"xtrlock\"" >&2
   exit 1
 fi
-set -x
+
 # Check for xrandr and run xterms
-if [ "$(command -v xrandr)" ]; then
 
-  # Calculate scaled offsets
-  ORIGINAL_RES="$(xrandr | grep -w connected | grep -oE '[0-9]{4}x[0-9]{4}' | head -n -1)"
-  SCALING="$(grep -m 1 "<scale>" ~/.config/monitors.xml  | grep -oE '[0-9]')"
-  OFFSETS="+0+0"
-  PREV_X=0
+# Calculate scaled offsets
+AMOUNT_SCREENS="$(xrandr | grep -wc connected)"
 
-  if [ -n "$ORIGINAL_RES" ] ; then
-    while read -r RES ; do
-      X=$((PREV_X + $(echo "$RES" | cut -d x -f1) / SCALING))
-      PREV_X="$X"
-      OFFSETS="${OFFSETS};+${X}+0"
-    done <<< "$ORIGINAL_RES"
-  fi
-
-  IFS=";"
-  for o in $OFFSETS; do
-     xfce4-terminal --geometry 0x0$o --fullscreen --hide-menubar \
-      --hide-scrollbar --hide-borders -e asciiquarium &
-  done
-else
-  xterm -fullscreen -e asciiquarium &
-fi
+for (( INDEX=1; INDEX<AMOUNT_SCREENS + 1; INDEX++ )) ; do
+  i3-msg 'workspace '"$INDEX"'; exec "xfce4-terminal --fullscreen --hide-menubar --hide-scrollbar --hide-borders -e asciiquarium"'
+done
 
 # Lock screen and kill all the child processes on unlock
 xtrlock && pkill -f asciiquarium
