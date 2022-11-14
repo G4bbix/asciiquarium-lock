aaaa719c914838f6669ebf694e4a4afbb67d39e9
G4bbix <winkiway@gmail.com>
2022-11-14 15:40:26 +0100

    Bug fix when only using one screen

--- asciiquarium-lock.sh
+++ asciiquarium-lock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Dependency checks
 if ! [ -f "/usr/local/bin/asciiquarium" ]; then
   echo "\"command not found: \"asciiquarium\"" >&2
@@ -14,21 +16,26 @@ if [ ! "$(command -v xtrlock)" ]; then
   exit 1
 fi
 
-WORKSPACES="$(i3-msg -t get_workspaces)"
-SCREENS_EXPR="$(jq -r ".[].output" <<< "$WORKSPACES" | sort -u | paste -sd \|)"
-WORKSPA0CES_EACH_SCREEN="$(jq -r 'keys[] as $k | "\(.[$k].output) \(.[$k].num)"' <<< "$WORKSPACES" \
-  | grep -E "^($SCREENS_EXPR)" | cut -d " " -f2)"
+# get one space from each screen
+WORKSPACES_TO_USE=""
+WORKSPACES_SCREENS="$(i3-msg -t get_workspaces | jq -r 'keys[] as $k | "\(.[$k].output) \(.[$k].num)"')"
+SCREENS="$(i3-msg -t get_workspaces | jq -r ".[].output" | sort -u)"
+while read -r SCREEN ; do
+  echo "$SCREEN"
+  WORKSPACES_TO_USE="$(grep "$SCREEN" <<< "$WORKSPACES_SCREENS" | head -n 1 | cut -d " " -f2) $WORKSPACES_TO_USE"
+done <<< "$SCREENS"
 
 while read -r SPACE ; do
+  echo "$SPACE"
   i3-msg 'workspace number '"$SPACE"''
-  i3-msg tabbed layout
+  i3-msg layout tabbed
   alacritty -e /usr/local/bin/asciiquarium &
   sleep .2
   i3-msg fullscreen toggle
-done <<< "$WORKSPA0CES_EACH_SCREEN"
+done <<< "$WORKSPACES_TO_USE"
 
 # Lock screen and kill all the child processes on unlock
  xtrlock && pkill -f asciiquarium && \
    while read -r SPACE ; do 
      \ i3-msg 'workspace number '"$INDEX"'' ; i3-msg fullscreen toggle
-   done <<< "$WORKSPA0CES_EACH_SCREEN"
+   done <<< "$WORKSPACES_TO_USE"
